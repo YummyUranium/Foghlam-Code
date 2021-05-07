06cb62f812eae75b3b5f3b70752a61274ba90c3a
YummyUranium <75385566+YummyUranium@users.noreply.github.com>
2021-05-07 15:30:10 +0100

    create canvas js code

---
+++ js/Pong.js
@@ -0,0 +1,14 @@
+// Declares canvas
+const canvas = document.getElementById("canvas");
+
+// Get context for drawing on canvas
+const ctx = canvas.getContext("2d");
+
+// Draws black canvas
+function render() {
+    // Sets the colour to fill
+    ctx.fillStyle = "black";
+
+    // Draws the black board
+    ctx.fillRect(0, 0, canvas.clientWidth, canvas.height);
+}
