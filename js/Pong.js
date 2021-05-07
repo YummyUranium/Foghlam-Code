5990682a79d22c9d7575ee913ab2fbadcd2c6167
YummyUranium <75385566+YummyUranium@users.noreply.github.com>
2021-05-07 15:44:22 +0100

    fix canvas colour

--- js/Pong.js
+++ js/Pong.js
@@ -4,10 +4,29 @@ const canvas = document.getElementById("canvas");
 // Get context for drawing on canvas
 const ctx = canvas.getContext("2d");
 
+// Variables
+const netWidth = 4;
+const netHeight = canvas.height;
+
+const paddleWidth = 10;
+const paddleHeight = 100;
+
+let upArrowPressed = false;
+let downArrowPressed = false;
+
+// Objects
+const net = {
+    x: canvas.width / 2 - netWidth / 2,
+    y: 0,
+    width: netWidth,
+    height: netHeight,
+
+}
+
 // Draws black canvas
 function render() {
     // Sets the colour to fill
-    ctx.fillStyle = "black";
+    ctx.fillStyle = "000";
 
     // Draws the black board
     ctx.fillRect(0, 0, canvas.clientWidth, canvas.height);
