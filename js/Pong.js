ba9eb81c5aa202ae06fe64fb269b8c1c230d1b5a
YummyUranium <75385566+YummyUranium@users.noreply.github.com>
2021-05-08 07:37:10 +0100

    add functions and run functions

--- js/Pong.js
+++ js/Pong.js
@@ -58,6 +58,41 @@ const ball = {
 }
 
 
+// Functions to draw objects
+function drawNet() {
+    // Colour of net:
+    ctx.fillStyle = net.color;
+
+    // Syntax -> fillRect(x, y, width, height)
+    ctx.fillRect(net.x, net.y, net.width, net.height);
+}
+
+// Draw Score
+function drawScore(x, y, score) {
+    ctx.fillStyle = "#FFF";
+    ctx.font = "35px sans-serif"
+
+    // Syntax -> fillText(text, x, y)
+    ctx.fillText(score, x, y);
+}
+
+// Draw Paddle
+function drawPaddle(x, y, width, height, color) {
+    ctx.fillStyle = color;
+    ctx.fillRect(x, y, width, height);
+}
+
+// Draw Ball
+function drawBall(x, y, radius, color) {
+    ctx.fillStyle = color;
+    ctx.beginPath();
+    // Syntax -> arc(x, y, radius, startAngle, endAngle, anti_clockwise_or_not)
+    ctx.arc(x, y, radius, 0, Math.PI * 2, true); // pi x 2 radians
+    ctx.closePath();
+    ctx.fill();
+}
+
+
 // Draws black canvas
 function render() {
     // Sets the colour to fill
@@ -65,6 +100,28 @@ function render() {
 
     // Draws the black board
     ctx.fillRect(0, 0, canvas.clientWidth, canvas.height);
+
+    // Draws the net
+    drawNet();
+
+
+    /*  The reason the scores are divided by four, and in the AI's case is then
+        multiplied is to quarter the screen up. */
+
+    // Draws user score
+    drawScore(canvas.width / 4, canvas.height / 6, user.score);
+
+    // Draws AI score
+    drawScore(3 * canvas.width / 4, canvas.height / 6, ai.score);
+
+    // Draws user paddle
+    drawPaddle(user.x, user.y, user.width, user.height, user.color);
+
+    // Draws AI paddle
+    drawPaddle(ai.x, ai.y, ai.width, ai.height, ai.color);
+
+    // Draws Ball
+    drawBall(ball.x, ball.y, ball.radius, ball.color);
 }
 
 render();
