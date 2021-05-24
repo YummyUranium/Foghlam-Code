02f6070dd6a14061cde604312591af80e4c5e143
YummyUranium <75385566+YummyUranium@users.noreply.github.com>
2021-05-24 10:13:46 +0100

    create two seperate decks, int and str

--- js/Blackjack.js
+++ js/Blackjack.js
@@ -1,81 +1,83 @@
 // BUILDING THE DECK
 
 // Declares arrays for card values
-const deck = [];
+var intDeck = [];
+var strDeck = [];
 
-// Adds 52 numbers to deck array
+// Adds 52 numbers to both deck arrays, renames strDeck numbers to strings
 for (var i = 1; i < 53; i++) {
+
+    intDeck.push(i);
 
     // Adds "Hearts" after the first 13 numbers
     if (i < 14) {
         if (i === 1) {
-            deck[i] = "Ace of Hearts";
+            strDeck[i] = "Ace of Hearts";
         } else if (i === 11) {
-            deck[i] = "Jack of Hearts";
+            strDeck[i] = "Jack of Hearts";
         } else if (i === 12) {
-            deck[i] = "Queen of Hearts";
+            strDeck[i] = "Queen of Hearts";
         } else if (i === 13) {
-            deck[i] = "King of Hearts";
+            strDeck[i] = "King of Hearts";
         } else {
-            deck[i] = i + " of Hearts";
+            strDeck[i] = i + " of Hearts";
         }
     }
     // Adds "Diamonds" after the next 13 numbers
     else if (i > 13 && i < 27) {
         var j = i - 13;
         if (j === 1) {
-            deck[i] = "Ace of Diamonds";
+            strDeck[i] = "Ace of Diamonds";
         } else if (j === 11) {
-            deck[i] = "Jack of Diamonds";
+            strDeck[i] = "Jack of Diamonds";
         } else if (j === 12) {
-            deck[i] = "Queen of Diamonds";
+            strDeck[i] = "Queen of Diamonds";
         } else if (j === 13) {
-            deck[i] = "King of Diamonds";
+            strDeck[i] = "King of Diamonds";
         } else {
-            deck[i] = j + " of Diamonds";
+            strDeck[i] = j + " of Diamonds";
         }
     }
     // Adds "Clubs" after the next 13 numbers
     else if (i > 26 && i < 40) {
         var k = i - 26;
         if (k === 1) {
-            deck[i] = "Ace of Clubs";
+            strDeck[i] = "Ace of Clubs";
         } else if (k === 11) {
-            deck[i] = "Jack of Clubs";
+            strDeck[i] = "Jack of Clubs";
         } else if (k === 12) {
-            deck[i] = "Queen of Clubs";
+            strDeck[i] = "Queen of Clubs";
         } else if (k === 13) {
-            deck[i] = "King of Clubs";
+            strDeck[i] = "King of Clubs";
         } else {
-            deck[i] = k + " of Clubs";
+            strDeck[i] = k + " of Clubs";
         }
     }
     // Adds "Spades" after the next 13 numbers
     else {
         var l = i - 39;
         if (l === 1) {
-            deck[i] = "Ace of Spades";
+            strDeck[i] = "Ace of Spades";
         } else if (l === 11) {
-            deck[i] = "Jack of Spades";
+            strDeck[i] = "Jack of Spades";
         } else if (l === 12) {
-            deck[i] = "Queen of Spades";
+            strDeck[i] = "Queen of Spades";
         } else if (l === 13) {
-            deck[i] = "King of Spades";
+            strDeck[i] = "King of Spades";
         } else {
-            deck[i] = l + " of Spades";
+            strDeck[i] = l + " of Spades";
         }
     }
 
-    deck.push(i);
+    strDeck.push(i);
 
 }
 
-// This is to remove first and last elements, the first
-// of which is empty and the second is 52, I don't know
-// why they exist but I can't be bothered to figure out
-// and this is a quick and easy fix.
-deck.pop();
-deck.shift();
+// Fix for weird elements at start and finish,
+// no it's not professional, but it works.
+strDeck.pop();
+strDeck.shift();
+
 
 // VARIABLES
 
@@ -89,11 +91,15 @@ var dealerCard1 = draw();
 var dealerCard2 = draw();
 var dealerHand = dealerCard1 + ", " + dealerCard2;
 
+// Variable for users hand value
+var userHandValue;
+
+
 // FUNCTIONS
 
 // Function for drawing cards
 function draw() {
-    return deck.splice(randArrayElem(deck), 1)[0];
+    return intDeck.splice(randArrayElem(intDeck), 1)[0];
 }
 
 // Print to <p> with id being id of html element you want to print to
@@ -121,6 +127,7 @@ function userTurn() {
 function userHit() {
     let userCard3 = draw();
     userHand = userHand + " and " + userCard3;
+    print("display", "You drew: " + userCard3 + ". Your hand is " + userHand + ". Value: ")
 }
 
 
@@ -130,7 +137,7 @@ function userHit() {
 print("welcome", "Welcome to Blackjack!");
 
 // Displayed first round
-print("display", "The dealer shuffles the deck and deals two cards to you. Your cards are: " + userCard1 + " and " + userCard2 + ". The dealer deals two cards to themselves, one of which is shown to you: " + dealerCard1 + ".\nWhat do you do?");
+print("display", "The dealer shuffles the deck and deals two cards to you. Your cards are: " + userCard1 + " and " + userCard2 + ". The dealer deals two cards to themselves, one of which is shown to you: " + dealerCard1 + ". The value of your hand is " + userHandValue + ".\nWhat do you do?");
 
 // Creates buttons for user's turn
 userTurn();
