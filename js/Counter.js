2e8d04a20b1d4511d69021bf054936c90c04a60d
YummyUranium <75385566+YummyUranium@users.noreply.github.com>
2022-05-05 10:56:51 +0100

    added new counter, linked proper js file

--- js/Counter.js
+++ js/Counter.js
@@ -1,12 +1,20 @@
-const pokeCount = document.getElementsByClassName("poke-count-number");
-const pokePlus = document.getElementsByClassName("plus");
-const pokeMinus = document.getElementsByClassName("minus");
+const turnCount = document.getElementsByClassName("turn-count-number");
+const turnPlus = document.getElementsByClassName("turn-plus");
+const turnMinus = document.getElementsByClassName("turn-minus");
 
-let pokeCountAmount = 0;
+const encounterCount = document.getElementsByClassName("encounter-count-number");
+const encounterPlus = document.getElementsByClassName("encounter-plus");
+const encounterMinus = document.getElementsByClassName("encounter-minus");
 
-pokePlus.addEventListener(onclick, addCounter(pokeCount, 1));
-pokeMinus.addEventListener(onclick, addCounter(pokeCount, -1));
+let turnCountAmount = 0;
+let encounterCountAmount = 0;
 
-function addCounter(counter, amount) {
-    counter.innerHTML = pokeCountAmount + amount;
+turnPlus.addEventListener(onclick, addCounter(turnCount, turnCountAmount, 1));
+turnMinus.addEventListener(onclick, addCounter(turnCount, turnCountAmount, -1));
+
+encounterPlus.addEventListener(onclick, addCounter(encounterCount, 1));
+encounterMinus.addEventListener(onclick, addCounter(encounterCount, encounterCountAmount, -1));
+
+function addCounter(counter,lastAmount, newAmount) {
+    counter.innerHTML = lastAmount + newAmount;
 }
