2d799ee29645f9dea921b1745cda9e6e9bbfd9a6
YummyUranium <75385566+YummyUranium@users.noreply.github.com>
2022-05-12 10:58:06 +0100

    added future game things

--- js/Space.js
+++ js/Space.js
@@ -4,3 +4,14 @@ const gameText = document.getElementById("story-text");
 window.onload() = function() {
     input.focus();
 }
+
+const gameCommands = ["open", "look", "attack", "talk", "roll"]
+
+let userTraits = {
+    strength = 10,
+    dexterity = 10,
+    constitution = 10,
+    charisma = 10,
+    intelligence = 10,
+    alignment = 5
+}
