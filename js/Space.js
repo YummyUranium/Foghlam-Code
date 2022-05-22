0cc50e5ca0a7d17a75515b47372c76e529bf5274
Eoghan <tbzphoenix@gmail.com>
2022-05-22 12:41:35 +0100

    added env traits

--- js/Space.js
+++ js/Space.js
@@ -8,22 +8,37 @@ window.onload() = function() {
 // Declaring variables
 const gameCommands = ["open", "look", "attack", "talk", "roll"]
 
+let env = {
+    temperature: 0,
+    humidity: 0,
+    weather: {
+        cloudy: false,
+        raining: false,
+        thunder: false,
+        hailing: false,
+        foggy: false
+    },
+    inside: true,
+    inSpace: false
+}
+
 let userTraits = {
-    strength = 10,
-    dexterity = 10,
-    constitution = 10,
-    charisma = 10,
-    intelligence = 10
+    strength: 10,
+    dexterity: 10,
+    constitution: 10,
+    charisma: 10,
+    intelligence: 10
 }
 
 // For creating Rooms - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes
 class Room {
-    constructor(env) {
+    constructor() {
         
     }
 }
 
 function increaseTrait(trait, amount) {
-    userTraits.trait += amount;
+    let trait = trait;
+    trait += amount;
 }
 
